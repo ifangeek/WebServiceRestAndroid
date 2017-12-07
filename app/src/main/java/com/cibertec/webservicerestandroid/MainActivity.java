5caf058ca76469808584de6f64b72f7d1a2b3f9c
iFanGeek <dpachecorosas96@gmail.com>
2017-12-07 17:21:39 -0500

    CONTINUAR LUEGO. CREANDO WS DE GRABAR.

--- app/src/main/java/com/cibertec/webservicerestandroid/MainActivity.java
+++ app/src/main/java/com/cibertec/webservicerestandroid/MainActivity.java
@@ -27,132 +27,157 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener,AdapterView.OnItemClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
     EditText edtfecha;
 
     Spinner splocal;
     ListView lst;
     Button btn;
-    ArrayList<LocalBean> locales=new ArrayList<LocalBean>();
-    ArrayList<PeliculaBean> peliculas=new ArrayList<PeliculaBean>();
+    ArrayList<LocalBean> locales = new ArrayList<LocalBean>();
+    //    ArrayList<PeliculaBean> peliculas=new ArrayList<PeliculaBean>();
     ProgressDialog pd;
-    String idlocal="",fecha="";
+    String idlocal = "", fecha = "";
+    ArrayAdapter<PeliculaBean> adapter;
 
-    private void IniciaComponente(){
-        edtfecha=(EditText)findViewById(R.id.edtFecha);
-        splocal=(Spinner)findViewById(R.id.spnLocal);
-        lst=(ListView)findViewById(R.id.lstPeliculas);
-        btn=(Button)findViewById(R.id.btnConsultar);
+
+    private void IniciaComponente() {
+        edtfecha = (EditText) findViewById(R.id.edtFecha);
+        splocal = (Spinner) findViewById(R.id.spnLocal);
+        lst = (ListView) findViewById(R.id.lstPeliculas);
+        btn = (Button) findViewById(R.id.btnConsultar);
         btn.setOnClickListener(this);
         lst.setOnItemClickListener(this);
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         IniciaComponente();
-        pd=ProgressDialog.show(this,"Conectando","Espere porfavor",true,false);
+        pd = ProgressDialog.show(this, "Conectando", "Espere porfavor", true, false);
         new Locales().execute();
+        adapter = new ArrayAdapter<PeliculaBean>(getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                new ArrayList<PeliculaBean>());
+        lst.setAdapter(adapter);
+
+
     }
+
     @Override
     public void onClick(View view) {
-        if(view == btn){
-            LocalBean l= (LocalBean)splocal.getItemAtPosition(splocal.getSelectedItemPosition());
-            idlocal =l.getIdlocal();
+
+        if (view == btn) {
+
+            LocalBean l = (LocalBean) splocal.getItemAtPosition(splocal.getSelectedItemPosition());
+            idlocal = l.getIdlocal();
             fecha = edtfecha.getText().toString();
-            pd=ProgressDialog.show(this,"Conectando","Espere porfavor",true,false);
+            pd = ProgressDialog.show(this, "Conectando", "Espere porfavor", true, false);
             new Peliculas().execute();
-            
+
+
         }
     }
+
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-        Intent ir=new Intent(this,BuscarPeliActivity.class);
-        PeliculaBean obj=(PeliculaBean)lst.getItemAtPosition(i);
-        ir.putExtra("obj",obj);
-        startActivity(ir);;
+        Intent ir = new Intent(this, BuscarPeliActivity.class);
+        PeliculaBean obj = (PeliculaBean) lst.getItemAtPosition(i);
+        ir.putExtra("obj", obj);
+        startActivity(ir);
+        ;
     }
-    public class Locales extends AsyncTask<String,Void,Object>{
+
+    public class Locales extends AsyncTask<String, Void, Object> {
         @Override
         protected Object doInBackground(String... strings) {
-            try{
-                HttpClient cliente= new DefaultHttpClient();
-                HttpContext ctx=new BasicHttpContext();
-                String URL="http://34.239.106.144:8080/restService/ListaLocales";
-                HttpGet get=new HttpGet(URL);
-                HttpResponse response = cliente.execute(get,ctx);
-                HttpEntity entity=response.getEntity();
-                String data = EntityUtils.toString(entity,"UTF-8");
-                JSONArray jsonobj =new JSONArray(data);
+            try {
+                HttpClient cliente = new DefaultHttpClient();
+                HttpContext ctx = new BasicHttpContext();
+                String URL = "http://34.239.106.144:8080/restService/ListaLocales";
+                HttpGet get = new HttpGet(URL);
+                HttpResponse response = cliente.execute(get, ctx);
+                HttpEntity entity = response.getEntity();
+                String data = EntityUtils.toString(entity, "UTF-8");
+                JSONArray jsonobj = new JSONArray(data);
 
-                Log.v("JSON",jsonobj.toString());
+                Log.v("JSON", jsonobj.toString());
                 LocalBean bean;
-                    JSONArray jsonarray=jsonobj;
-                    for(int i=0;i<jsonarray.length();i++){
-                        JSONObject jsonrow=jsonarray.getJSONObject(i);
-                        bean=new LocalBean();
-                        bean.setIdlocal(jsonrow.getString("idlocal"));
-                        bean.setNnomlocal(jsonrow.getString("nomlocal"));
-                        locales.add(bean);
-                    }
+                JSONArray jsonarray = jsonobj;
+                for (int i = 0; i < jsonarray.length(); i++) {
+                    JSONObject jsonrow = jsonarray.getJSONObject(i);
+                    bean = new LocalBean();
+                    bean.setIdlocal(jsonrow.getString("idlocal"));
+                    bean.setNnomlocal(jsonrow.getString("nomlocal"));
+                    locales.add(bean);
+                }
 
-            }catch(Exception ex){
+            } catch (Exception ex) {
                 ex.printStackTrace();
             }
             return null;
         }
-        protected void onPostExecute(Object result){
-            ArrayAdapter<LocalBean> adapter=new ArrayAdapter<LocalBean>(getApplicationContext(),
+
+        protected void onPostExecute(Object result) {
+
+            ArrayAdapter<LocalBean> adapter = new ArrayAdapter<LocalBean>(getApplicationContext(),
                     android.R.layout.simple_list_item_1,
                     locales);
             splocal.setAdapter(adapter);
             pd.dismiss();
+
+
         }
     }
 
-    public class Peliculas extends AsyncTask<String,Void,Object>{
+    public class Peliculas extends AsyncTask<String, Void, ArrayList<PeliculaBean>> {
         @Override
-        protected Object doInBackground(String... strings) {
-            try{
-                HttpClient cliente= new DefaultHttpClient();
-                HttpContext ctx=new BasicHttpContext();
-                String URL="http://34.239.106.144:8080/restService/consultaPelicula?tbLocal="+idlocal+"&fecha="+fecha;
-                Log.v("URL",URL);
-                HttpGet get=new HttpGet(URL);
-                HttpResponse response = cliente.execute(get,ctx);
-                HttpEntity entity=response.getEntity();
-                String data = EntityUtils.toString(entity,"UTF-8");
-                JSONArray jsonobj =new JSONArray(data);
+        protected ArrayList<PeliculaBean> doInBackground(String... strings) {
+            ArrayList<PeliculaBean> lstPeliculasBean = new ArrayList<>();
+            try {
+                HttpClient cliente = new DefaultHttpClient();
+                HttpContext ctx = new BasicHttpContext();
+                String URL = "http://34.239.106.144:8080/restService/consultaPelicula?tbLocal=" + idlocal + "&fecha=" + fecha;
+                Log.v("URL", URL);
+                HttpGet get = new HttpGet(URL);
+                HttpResponse response = cliente.execute(get, ctx);
+                HttpEntity entity = response.getEntity();
+                String data = EntityUtils.toString(entity, "UTF-8");
+                JSONArray jsonobj = new JSONArray(data);
                 PeliculaBean bean;
                 LocalBean beanlocal;
-                    JSONArray jsonarray=jsonobj;
-                        for(int i=0;i<jsonarray.length();i++){
-                        JSONObject jsonrow = jsonarray.getJSONObject(i);
-                        bean = new PeliculaBean();
-                        Log.v("IDPELICULA",bean.getIdpelicula()+"");
-                        bean.setIdpelicula(jsonrow.getInt("codPelicula"));
-                        bean.setNombre(jsonrow.getString("nombre"));
-                        bean.setSala(jsonrow.getString("sala"));
-                        bean.setInicio(jsonrow.getString("inicio"));
-                        bean.setFin(jsonrow.getString("fin"));
-                        JSONObject jsonlocal = jsonrow.getJSONObject("tbLocal");
-                        Log.v("JSON",jsonlocal+"");
-                        bean.setIdlocal(jsonlocal.getString("nomlocal"));
-                        peliculas.add(bean);
-                    }
+                JSONArray jsonarray = jsonobj;
+                for (int i = 0; i < jsonarray.length(); i++) {
+                    JSONObject jsonrow = jsonarray.getJSONObject(i);
+                    bean = new PeliculaBean();
+                    Log.v("IDPELICULA", bean.getIdpelicula() + "");
+                    bean.setIdpelicula(jsonrow.getInt("codPelicula"));
+                    bean.setNombre(jsonrow.getString("nombre"));
+                    bean.setSala(jsonrow.getString("sala"));
+                    bean.setInicio(jsonrow.getString("inicio"));
+                    bean.setFin(jsonrow.getString("fin"));
+                    JSONObject jsonlocal = jsonrow.getJSONObject("tbLocal");
+                    Log.v("JSON", jsonlocal + "");
+                    bean.setIdlocal(jsonlocal.getString("nomlocal"));
+                    lstPeliculasBean.add(bean);
+                }
 
-            }catch(Exception ex){
+            } catch (Exception ex) {
                 ex.printStackTrace();
+                lstPeliculasBean.clear();
             }
-            return null;
+            return lstPeliculasBean;
         }
-        protected void onPostExecute(Object result){
-            ArrayAdapter<PeliculaBean> adapter=new ArrayAdapter<PeliculaBean>(getApplicationContext(),
-                    android.R.layout.simple_list_item_1,
-                    peliculas);
-            lst.setAdapter(adapter);
+
+        protected void onPostExecute(ArrayList<PeliculaBean> result) {
+            adapter.clear();
+            adapter.addAll(result);
+            adapter.notifyDataSetChanged();
+
+
             pd.dismiss();
+
         }
     }
 }
